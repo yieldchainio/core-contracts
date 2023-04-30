d83cb65581da9b369dba4044ea0aa3b2f2c328ff
OfirYC <ofir@yieldchain.io>
2023-04-30 06:44:37 +0300

    :sparkles: Structs, Fixed-length arrays working w/ initial manual testing
    Seems like it's working for now. I am trying to compile it now and deploy on mainnet so i can try with some real external contracts... once i get a test or 2 done im wrting unit tests and moving onto vault logic

--- new-src/vm/VM.sol
+++ new-src/vm/VM.sol
@@ -25,7 +25,7 @@ contract YCParsers is YieldchainTypes, Interpreters {
         );
 
         /**
-         * Assert that the typeflag must be either 0x04, 0x05, or 0x06
+         * Assert that the typeflag must be either 0x04, 0x05, or 0x06 (The function call flags)
          */
         require(
             typeflag < 0x07 && typeflag > 0x03,
@@ -153,12 +153,12 @@ contract YCParsers is YieldchainTypes, Interpreters {
              * the returned typeflag (which should be the typeflag of the underlying return value)
              * Note that, to avoid any doubts -
              * The underlying typeflag in this case should always just be the return type flag of the function call,
-             * that we input into the function. It's just the uniform API of the function that requires us to receive
-             * it anyway.
+             * that we input into the function. It's just the uniform API of the function that makes it more efficient
+             * to receive it from this call anyway.
              *
              * The additional interpretation is done in order to comply the primitive underlying return value
              * with the rest of the system (i.e chunck/calldata encoder). For example, if the function returns
-             * a dynamic variable - We need to remove it's initial 32-byte offset pointer in order for it to
+             * a ref variable - We need to remove it's initial 32-byte offset pointer in order for it to
              * be compliant with the calldata builder.
              */
 
@@ -204,12 +204,12 @@ contract YCParsers is YieldchainTypes, Interpreters {
         typeFlag = typeflag;
 
         /**
-         * If the typeflag is 0x00, it's a static variable and we just return it (simplest case)
+         * If the typeflag is 0x00, it's a value variable and we just return it (simplest case)
          */
         if (typeflag == VALUE_VAR_FLAG) return (parsedPrimitiveValue, typeflag);
 
         /**
-         * If the typeflag is 0x01, it's a dynamic-type variable (string, array...), we parse and return it
+         * If the typeflag is 0x01, it's a ref variable (string, array...), we parse and return it
          */
         if (typeflag == REF_VAR_FLAG) {
             parsedPrimitiveValue = _parseDynamicVar(parsedPrimitiveValue);
@@ -219,7 +219,7 @@ contract YCParsers is YieldchainTypes, Interpreters {
         /**
          * If the typeflag equals to the commands array flag, we call the interpretCommandsArray() function,
          * which will iterate over each item, parse it, and in addition, have some more utility parsings
-         * depending on it's typeflag.
+         * depending on it's typeflag (e.g appending/not appending additional length argument, etc).
          *
          * We also return the return typeflag as the typeflag here.
          */
@@ -237,7 +237,7 @@ contract YCParsers is YieldchainTypes, Interpreters {
      * @notice
      * interpretCommandsAndEncodeChunck
      * Accepts an array of YC commands - interprets each one of them, then encodes an ABI-compatible chunck of bytes,
-     * corresponding of all of these arguments (account for static & dynamic variables)
+     * corresponding of all of these arguments (account for value & ref variables)
      * @param ycCommands - an array of yc commands to interpret
      * @return interpretedEncodedChunck - A chunck of bytes which is an ABI-compatible encoded version
      * of all of the interpreted commands
@@ -246,40 +246,38 @@ contract YCParsers is YieldchainTypes, Interpreters {
         bytes[] memory ycCommands
     ) public returns (bytes memory interpretedEncodedChunck) {
         /**
-         * @notice
-         * We keep track of all of the dynamic variables we have on the commands,
-         * in order to encode them correctly.
-         */
-
-        /**
-         * We begin by getting the amount of all dynamic variables,
+         * We begin by getting the amount of all ref variables,
          * in order to instantiate the array.
          *
          * Note that we are looking at the RETURN typeflag of the command at idx 1,
          * and we're doing it since a command may be flagged as some certain type in order to be parsed correctly,
-         * but the end result we will be getting from the parsing iteration is different - for example, dynamic
-         * commands arrays (dynamic-length arrays which are made up of YC commands) are flagged as 0x03 in order to
-         * be parsed differently, yet at the end we're supposed to get a reguler dynamic flag from the parsing
-         * (Since that is what the end contract expects - a dynamic-length variable which is some array).
-         *
-         * This means that for a dynamic variable to be flagged correctly, it's return type need to be flagged
-         * also as dynamic (0x01)
+         * but the end result, the underlaying value we will be getting from the parsing iteration is different.
+         * For example, dynamic-length commands arrays (dynamic-length arrays which are made up of YC commands)
+         * are flagged as 0x03 in order to be parsed in a certain way, yet at the end we're supposed to get a
+         * reguler dynamic flag from the parsing (Since that is what the end contract expects,
+         * a dynamic-length argument which is some array).
+         * This means that for a ref variable to be flagged correctly, it's return type need to be flagged
+         * also as REF_VAR_FLAG (0x01)
          */
-        uint256 dynamicVarsAmt = 0;
+        uint256 refVarsAmt = 0;
         for (uint256 i = 0; i < ycCommands.length; i++) {
-            if (ycCommands[i][1] == REF_VAR_FLAG) ++dynamicVarsAmt;
+            if (ycCommands[i][1] == REF_VAR_FLAG) ++refVarsAmt;
         }
 
-        // The actual array of dynamic-length variables
-        bytes[] memory dynamicVars = new bytes[](dynamicVarsAmt);
+        /**
+         * Will save the ref variables' body values/data here
+         */
+        bytes[] memory refVars = new bytes[](refVarsAmt);
 
-        // Their indexes within the chunck
-        uint256[] memory dynamicVarsIndexes = new uint256[](dynamicVarsAmt);
+        /**
+         * The indexes of the ref variables' offset pointers
+         */
+        uint256[] memory refVarsIndexes = new uint256[](refVarsAmt);
 
-        // We save a uint256 variable to keep track of the current available index
-        // within the array of dynamic variables. This is because we cannot push into
-        // in-memory arrays in Solidity
-        uint256 freeDynVarIndexPtr = 0;
+        /**
+         * Keep a uint in order to track the current free idx in the array (cannot push to mem arrays in solidity)
+         */
+        uint256 freeRefVarIndexPtr = 0;
 
         /**
          * Iterate over each one of the ycCommands,
@@ -295,14 +293,14 @@ contract YCParsers is YieldchainTypes, Interpreters {
             ) = _separateAndGetCommandValue(ycCommands[i]);
 
             /**
-             * Assert that the typeflag must either be a static or a dynamic variable.
-             * At this point, the argument should have been interpreted up until the point where
-             * it's either dynamic or static length variable.
+             * Assert that the typeflag must either be a value or a ref variable.
+             * At this point, the argument should have been interpreted/parsed up until the point where
+             * it's either a ref or a value variable.
              */
             require(typeflag < 0x02, "typeflag must < 2 after parsing");
 
             /**
-             * If it's a static variable, we simply concat the existing chunck with it
+             * If it's a value variable, we simply concat the existing chunck with it
              */
             if (typeflag == VALUE_VAR_FLAG)
                 interpretedEncodedChunck = bytes.concat(
@@ -311,16 +309,15 @@ contract YCParsers is YieldchainTypes, Interpreters {
                 );
 
                 /**
-                 * Otherwise, we process it as a dynamic variable
+                 * Otherwise, we process it as a ref variable
                  */
             else {
                 /**
-                 * We save the current chunck length as the index of the 32 byte pointer of this dynamic variable,
-                 * in our array of dynamicVarIndexes
+                 * We save the current chunck length as the index of the 32 byte pointer of this ref variable,
+                 * in our array of refVarIndexes
                  */
-                dynamicVarsIndexes[
-                    freeDynVarIndexPtr
-                ] = interpretedEncodedChunck.length;
+                refVarsIndexes[freeRefVarIndexPtr] = interpretedEncodedChunck
+                    .length;
 
                 /**
                  * We then append an empty 32 byte placeholder at that index on the chunck
@@ -333,33 +330,33 @@ contract YCParsers is YieldchainTypes, Interpreters {
 
                 /**
                  * We then, at the same index as we saved the chunck pointer's index,
-                 * save the parsed value of the dynamic argument (it was parsed to be just the length + data
-                 * by the getCommandValue() function, it does not include the default prepended mem pointer now).
+                 * save the parsed value of the ref argument (it was parsed to be just the length + data
+                 * by the getCommandValue() function, it does not include the default prepended offset pointer now).
                  */
-                dynamicVars[freeDynVarIndexPtr] = argumentValue;
+                refVars[freeRefVarIndexPtr] = argumentValue;
 
                 // Increment the free index pointer of the dynamic variables
-                ++freeDynVarIndexPtr;
+                ++freeRefVarIndexPtr;
             }
         }
 
         /**
          * @notice,
          * at this point we have iterated over each command.
-         * The fixed-length arguments were concatinated with our chunck,
-         * whilst the dynamic variables have been replaced with an empty 32 byte at their index,
-         * and their values & indexes of these empty placeholders were saved into our arrays.
+         * The value arguments were concatinated with our chunck,
+         * whilst the ref variables have been replaced with an empty 32 byte placeholder at their index,
+         * their values & the indexes of their empty placeholders were saved into our arrays.
          *
          * We now perform an additional iteration over these arrays, where we append the
-         * dynamic variables to the end of the encoded chunck, save that new index of where we appended it,
+         * ref variables to the end of the encoded chunck, save that new index of where we appended it,
          * go back to the index of the corresponding empty placeholder, and replace it with a pointer to our new index.
          *
          * the EVM, when accepting this chunck as calldata, will expect this memory pointer at the index, which, points
          * to where our variable is located in terms of offset since the beginning of the chunck
          */
-        for (uint256 i = 0; i < dynamicVars.length; i++) {
+        for (uint256 i = 0; i < refVars.length; i++) {
             // Shorthand for the index of our placeholder pointer
-            uint256 index = dynamicVarsIndexes[i];
+            uint256 index = refVarsIndexes[i];
 
             // The new index/pointer
             uint256 newPtr = interpretedEncodedChunck.length;
@@ -369,18 +366,15 @@ contract YCParsers is YieldchainTypes, Interpreters {
                 mstore(add(add(interpretedEncodedChunck, 0x20), index), newPtr)
             }
 
-            // Finally, concat the existing chunck with our dynamic variable's length + data
-            // (At what would now be stored in the  original index
-            // as the mem pointer)
+            /**
+             * Finally, concat the existing chunck with our ref variable's data
+             * (At what would now be stored in the original index as the offset pointer)
+             */
             interpretedEncodedChunck = bytes.concat(
                 interpretedEncodedChunck,
-                dynamicVars[i]
+                refVars[i]
             );
         }
-        /**
-         * Finally, concat the 4 byte function selector with the arguments body - and return it
-         */
-        return interpretedEncodedChunck;
     }
 
     /**
@@ -416,12 +410,12 @@ contract YCParsers is YieldchainTypes, Interpreters {
          * We check to see our provided typeflag,
          * which is supposed to be the typeflag of this command (the array of commands as a whole).
          *
-         * If the return typeflag is a COMMANDS_REF_ARR_FLAG,
+         * If the typeflag is a COMMANDS_REF_ARR_FLAG,
          * this means we need to concat the length of the array to the chunck (because it's a dynamic-length array).
          * Otherwise, it is either a fixed-length array, or a struct. In which case, we do not append any length.
          *
-         * Do note that, in neither cases, we append a memory pointer. The calldata builder expects
-         * a "naked" value (length (optionally) + data), and manages the offset pointers itself.
+         * Do note that, in neither cases, we append an offset pointer. The calldata builder expects
+         * a "naked" value ((optional length) + data), and manages the offset pointers itself.
          */
         if (typeflag == COMMANDS_REF_ARR_FLAG) {
             interpretedArray = bytes.concat(
