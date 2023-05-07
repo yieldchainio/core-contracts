cdcd2ede2b29e72c80d409040984d64fbfd640c6
OfirYC <ofir@yieldchain.io>
2023-05-07 19:51:48 +0300

    :wastebasket: Cleaned up old src

--- old-src/yc-diamond/YC-Diamond.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
-
-import "./DiamondBase.sol";
-
-abstract contract YieldchainDiamond is DiamondBase {}
--- old-src/yc-diamond/interfaces/IArrayMethods.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IArrayMethods {
-    // Find a uint inside an array of uints
-    function findUint(uint256[] memory, uint256) external pure returns (bool);
-}
