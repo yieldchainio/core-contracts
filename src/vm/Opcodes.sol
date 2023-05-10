d0caff012edc6fbf5d0e56d26792f778b3959f53
OfirYC <ofir@yieldchain.io>
2023-05-10 08:08:28 +0300

    :sparkles: `extractWordAtIndex()` util opcode

--- src/vm/Opcodes.sol
+++ src/vm/Opcodes.sol
@@ -27,4 +27,19 @@ contract Opcodes {
             firstWord := mload(add(arg, 0x20))
         }
     }
+
+    /**
+     * encodeWordAtIndex()
+     * @param arg - The original arg to extract from
+     * @param idx - The index of the word to extract (starts at 0 for first word)
+     * @return extractedWord - 32 byte at the index
+     */
+    function extractWordAtIndex(
+        bytes memory arg,
+        uint256 idx
+    ) public pure returns (bytes32 extractedWord) {
+        assembly {
+            extractedWord := mload(add(arg, add(0x20, mul(0x20, idx))))
+        }
+    }
 }
